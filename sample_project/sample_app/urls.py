273a4d820b15c0b7716726d0e1cef8bdcb8f1357
mykysyk <mykysyk@gmail.com>
2019-04-27 22:57:36 +0900

    REST frameworkの設定をアプリ用URLConfに組み込む

--- sample_project/sample_app/urls.py
+++ sample_project/sample_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from . import views
+from rest_framework import routers
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+router.register(r'memo', views.MemoViewSet)
